4091c9b4da05ff7a69d4d4b42e263430a403918a
Paulo Victor <148218637+PauloVictorRodrigues@users.noreply.github.com>
2023-11-09 11:09:39 -0300

    Implementação dos métodos na ProdutoBLL

--- Academia/BLL/ProdutoBLL.cs
+++ Academia/BLL/ProdutoBLL.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DAL;
+using Models;
 
 namespace BLL
 {
-    internal class ProdutoBLL
+    public class ProdutoBLL
     {
+        public void Inserir(Produto _produto)
+        {
+            if (string.IsNullOrEmpty(_produto.Nome))
+                throw new Exception("Informe o nome do produto.");
+
+            new ProdutoDAL().Inserir(_produto);
+        }
+        public void Alterar(Produto _produto)
+        {
+            new ProdutoDAL().Alterar(_produto);
+        }
+        public void Excluir(int _id)
+        {
+            new ProdutoDAL().Excluir(_id);
+        }
+        public List<Produto> BuscarTodos()
+        {
+            return new ProdutoDAL().BuscarTodos();
+        }
+        public Produto BuscarPorId(int _id)
+        {
+            return new ProdutoDAL().BuscarPorId(_id);
+        }
+        public Produto BuscarPorCodigoDeBarras(string _codigoDeBarras)
+        {
+            return new ProdutoDAL().BuscarPorCodigoDeBarras(_codigoDeBarras);
+        }
+        public List<Produto> BuscarPorNome(string _nome)
+        {
+            return new ProdutoDAL().BuscarPorNome(_nome);
+        }
     }
 }
--- Academia/DAL/ProdutoDAL.cs
+++ Academia/DAL/ProdutoDAL.cs
@@ -1,11 +1,6 @@
 ﻿using Models;
-using System;
-using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DAL
 {
