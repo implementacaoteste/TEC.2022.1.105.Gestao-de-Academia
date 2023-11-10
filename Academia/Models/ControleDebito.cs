b30f3f5f65d67b91f0f7c803ea3b6d4500931489
Guilherme De Jesus Rosa <148218502+Guilhermejr02@users.noreply.github.com>
2023-11-10 08:06:57 -0300

    Criação da classe ControleDebito na Models

--- Academia/Models/ControleDebito.cs
+++ Academia/Models/ControleDebito.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Models
+﻿namespace Models
 {
-    internal class ControleDebito
+    public class ControleDebito
     {
+        public int Id { get; set; }
+        public int clienteId { get; set; }
+        public bool Estatus { get; set; }
+        public double ValorDebito { get; set; }
+        public double FormaPagamento { get; set; }
+        public DateTime DataLancamento { get; set; }
+        public DateTime DataVencimento { get; set; }
+        public DateTime DataPagamento { get; set; }
     }
 }
