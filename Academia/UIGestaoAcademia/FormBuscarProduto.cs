0e125bf928a8cc9649ef7207c776d245f8ccc367
Paulo Victor <148218637+PauloVictorRodrigues@users.noreply.github.com>
2023-11-10 08:18:47 -0300

    Implementação do método Excluir, Alterar, Buscar e Inserir no FormBuscarProduto

--- Academia/UIGestaoAcademia/FormBuscarProduto.cs
+++ Academia/UIGestaoAcademia/FormBuscarProduto.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BLL;
+using Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,5 +18,55 @@ namespace UIGestaoAcademia
         {
             InitializeComponent();
         }
+
+        private void buttonBuscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                switch (comboBoxBuscarPor.SelectedIndex)
+                {
+                    case 0:
+                        bindingSourceProduto.DataSource = new ProdutoBLL().BuscarPorNome(textBoxBuscarPor.Text);
+                        break;
+                    case 1:
+                        bindingSourceProduto.DataSource = new ProdutoBLL().BuscarPorCodigoDeBarras(textBoxBuscarPor.Text);
+                        break;
+                    default:
+                        bindingSourceProduto.DataSource = new ProdutoBLL().BuscarTodos();
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void buttonAlterar_Click(object sender, EventArgs e)
+        {
+            int id = ((Produto)bindingSourceProduto.Current).Id;
+
+            using (FormCadastrarProduto frm = new FormCadastrarProduto(id))
+            {
+                frm.ShowDialog();
+            }
+        }
+        private void buttonInserir_Click(object sender, EventArgs e)
+        {
+            using (FormCadastrarProduto frm = new FormCadastrarProduto())
+            {
+                frm.ShowDialog();
+            }
+        }
+        private void buttonExcluir_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja realmente excluir esse registro?", "Atenção", MessageBoxButtons.YesNo) == DialogResult.No)
+                return;
+
+            int id = ((Produto)bindingSourceProduto.Current).Id;
+            new ProdutoBLL().Excluir(id);
+            bindingSourceProduto.RemoveCurrent();
+            MessageBox.Show("Registro excluido com sucesso!");
+        }
+
     }
 }
