65ad7294d4ae0e400e0b99e0ed17312a7b0d7878
Gustavo Henrique <148218604+GustavoHenriquePC@users.noreply.github.com>
2023-11-10 11:06:40 -0300

    Criado a ModelFornecedor

--- Academia/UIGestaoAcademia/FormLogin.Designer.cs
+++ Academia/UIGestaoAcademia/FormLogin.Designer.cs
@@ -39,26 +39,29 @@
             // 
             // textBoxUsuario
             // 
-            this.textBoxUsuario.Location = new System.Drawing.Point(12, 77);
+            this.textBoxUsuario.Location = new System.Drawing.Point(12, 96);
+            this.textBoxUsuario.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBoxUsuario.Name = "textBoxUsuario";
-            this.textBoxUsuario.Size = new System.Drawing.Size(366, 22);
+            this.textBoxUsuario.Size = new System.Drawing.Size(366, 27);
             this.textBoxUsuario.TabIndex = 2;
             this.textBoxUsuario.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxUsuario_KeyDown);
             // 
             // textBoxSenha
             // 
-            this.textBoxSenha.Location = new System.Drawing.Point(12, 138);
+            this.textBoxSenha.Location = new System.Drawing.Point(12, 172);
+            this.textBoxSenha.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBoxSenha.Name = "textBoxSenha";
             this.textBoxSenha.PasswordChar = '*';
-            this.textBoxSenha.Size = new System.Drawing.Size(366, 22);
+            this.textBoxSenha.Size = new System.Drawing.Size(366, 27);
             this.textBoxSenha.TabIndex = 4;
             this.textBoxSenha.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxSenha_KeyDown);
             // 
             // buttonEntrar
             // 
-            this.buttonEntrar.Location = new System.Drawing.Point(103, 182);
+            this.buttonEntrar.Location = new System.Drawing.Point(103, 228);
+            this.buttonEntrar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.buttonEntrar.Name = "buttonEntrar";
-            this.buttonEntrar.Size = new System.Drawing.Size(75, 29);
+            this.buttonEntrar.Size = new System.Drawing.Size(75, 36);
             this.buttonEntrar.TabIndex = 5;
             this.buttonEntrar.Text = "&Entrar";
             this.buttonEntrar.UseVisualStyleBackColor = true;
@@ -67,9 +70,10 @@
             // button2
             // 
             this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.button2.Location = new System.Drawing.Point(213, 182);
+            this.button2.Location = new System.Drawing.Point(213, 228);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 29);
+            this.button2.Size = new System.Drawing.Size(75, 36);
             this.button2.TabIndex = 6;
             this.button2.Text = "&Cancelar";
             this.button2.UseVisualStyleBackColor = true;
@@ -77,37 +81,37 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 119);
+            this.label1.Location = new System.Drawing.Point(12, 149);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 16);
+            this.label1.Size = new System.Drawing.Size(49, 20);
             this.label1.TabIndex = 3;
             this.label1.Text = "Senha";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 58);
+            this.label2.Location = new System.Drawing.Point(12, 72);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(54, 16);
+            this.label2.Size = new System.Drawing.Size(59, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Usuário";
             // 
             // label4
             // 
             this.label4.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label4.Location = new System.Drawing.Point(0, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(390, 58);
+            this.label4.Size = new System.Drawing.Size(390, 72);
             this.label4.TabIndex = 0;
             this.label4.Text = "Login";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // FormLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(390, 237);
+            this.ClientSize = new System.Drawing.Size(390, 296);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -117,6 +121,7 @@
             this.Controls.Add(this.textBoxUsuario);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FormLogin";
@@ -124,6 +129,7 @@
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
+            this.Load += new System.EventHandler(this.FormLogin_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormLogin_KeyDown);
             this.ResumeLayout(false);
             this.PerformLayout();
