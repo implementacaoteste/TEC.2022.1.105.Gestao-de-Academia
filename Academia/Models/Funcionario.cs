56275239529164d2bcbf4172727cfe29e13bfd2a
Paulo Victor <148218637+PauloVictorRodrigues@users.noreply.github.com>
2023-11-20 08:30:26 -0300

    Implementação da variável Cargo na classe Funcionario

--- Academia/Models/Funcionario.cs
+++ Academia/Models/Funcionario.cs
@@ -6,6 +6,7 @@ namespace Models
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Cpf { get; set; }
+        public string Cargo { get; set; }
         public string Telefone { get; set; }
         public string Email { get; set; }
         public string Endereco { get; set; }
