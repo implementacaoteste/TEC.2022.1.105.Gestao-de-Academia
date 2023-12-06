80cf29a526d475c134ea69e065bfd000d24c73bc
Gustavo Henrique <148218604+GustavoHenriquePC@users.noreply.github.com>
2023-12-06 08:57:15 -0300

    Implementação da Classe TipoDeContaBLL e seus métodos Inserir e Alterar, a mesma coisa foi feito na Classe TipoDeContaDAL

---
+++ Academia/BLL/TipoDeContaBLL.cs
@@ -0,0 +1,23 @@
+﻿using DAL;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class TipoDeContaBLL
+    {
+        public void Inserir(TipoDeConta _TipoDeConta)
+        {
+            new TipoDeContaDAL().Inserir(_TipoDeConta);
+        }
+        public void Alterar(TipoDeConta _TipoDeConta)
+        {
+            new TipoDeContaDAL().Alterar(_TipoDeConta);
+        }
+
+    }
+}
