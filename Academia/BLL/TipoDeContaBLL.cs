020c2733233e6d28c4c73928672937682709d351
Gustavo Henrique <148218604+GustavoHenriquePC@users.noreply.github.com>
2023-12-06 10:12:15 -0300

    Implementação da FormCadastrarTipoDeConta e seus botões

--- Academia/BLL/TipoDeContaBLL.cs
+++ Academia/BLL/TipoDeContaBLL.cs
@@ -10,14 +10,21 @@ namespace BLL
 {
     public class TipoDeContaBLL
     {
-        public void Inserir(TipoDeConta _TipoDeConta)
+        private void Inserir(TipoDeConta _TipoDeConta)
         {
             new TipoDeContaDAL().Inserir(_TipoDeConta);
         }
-        public void Alterar(TipoDeConta _TipoDeConta)
+        private void Alterar(TipoDeConta _TipoDeConta)
         {
             new TipoDeContaDAL().Alterar(_TipoDeConta);
         }
 
+        public void Salvar(TipoDeConta _tipoDeConta)
+        {
+            if (_tipoDeConta.Id == 0)
+                Inserir(_tipoDeConta);
+            else
+                Alterar(_tipoDeConta);
+        }
     }
 }
