2f8bf472164de825df06cbd3e9ac9e1b6bc8115e
Robson Souza <148218643+RobsonDSouza@users.noreply.github.com>
2023-12-12 08:29:35 -0300

    Implementação no formExercicio

--- Academia/UIGestaoAcademia/FormBuscarExercicio.cs
+++ Academia/UIGestaoAcademia/FormBuscarExercicio.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BLL;
+using Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,76 @@ namespace UIGestaoAcademia
 {
     public partial class FormBuscarExercicio : Form
     {
+        public Exercicio Exercicio { get; private set; }
+
         public FormBuscarExercicio()
         {
             InitializeComponent();
+        }
+
+        private void buttonInserir_Click(object sender, EventArgs e)
+        {
+            using (FormCadastrarExercicio frm = new FormCadastrarExercicio())
+            {
+                frm.ShowDialog();
+            }
+        }
+
+        private void buttonBuscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                switch (comboBoxBuscarPor.SelectedIndex)
+                {
+                    case 0:
+                        exercicioBindingSource.DataSource = new ProdutoBLL().BuscarPorNome(textBoxBuscarPor.Text);
+                        break;
+                    default:
+                        exercicioBindingSource.DataSource = new ProdutoBLL().BuscarTodos();
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void buttonAlterar_Click(object sender, EventArgs e)
+        {
+            int id = ((Exercicio)exercicioBindingSource.Current).Id;
+
+            using (FormCadastrarExercicio frm = new FormCadastrarExercicio(id))
+            {
+                frm.ShowDialog();
+            }
+        }
+
+        private void buttonExcluir_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja realmente excluir esse registro?", "Atenção", MessageBoxButtons.YesNo) == DialogResult.No)
+                return;
+
+            int id = ((Exercicio)exercicioBindingSource.Current).Id;
+            new ProdutoBLL().Excluir(id);
+            exercicioBindingSource.RemoveCurrent();
+            MessageBox.Show("Registro excluido com sucesso!");
+        }
+
+        private void buttonSelecionar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (exercicioBindingSource.Count == 0)
+                    throw new Exception("Não existe registro para ser selecionado!");
+
+                this.Exercicio = (Exercicio)exercicioBindingSource.Current;
+
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
