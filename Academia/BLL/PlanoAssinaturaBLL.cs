89efdaabed75d1fd48efa6c08939032e0188553c
Paulo Victor <148218637+PauloVictorRodrigues@users.noreply.github.com>
2024-01-25 08:49:43 -0300

    Criação dos métodos Inserir, Alterar, Excluir, BuscarTodos, BuscarPorId no PlanoAssinaturaBLL

---
+++ Academia/BLL/PlanoAssinaturaBLL.cs
@@ -0,0 +1,30 @@
+﻿using DAL;
+using Models;
+
+
+namespace BLL
+{
+    public class PlanoAssinaturaBLL
+    {
+        public void Inserir(PlanoAssinatura _planoAssinatura)
+        {
+            new PlanoAssinaturaDAL().Inserir(_planoAssinatura);
+        }
+        public void Alterar(PlanoAssinatura _planoAssinatura)
+        {
+            new PlanoAssinaturaDAL().Alterar(_planoAssinatura);
+        }
+        public void Excluir(int _id)
+        {
+            new PlanoAssinaturaDAL().Excluir(_id);
+        }
+        public List<PlanoAssinatura> BuscarTodos()
+        {
+            return new PlanoAssinaturaDAL().BuscarTodos();
+        }
+        public PlanoAssinatura BuscarPorId(int _id)
+        {
+            return new PlanoAssinaturaDAL().BuscarPorId(_id);
+        }
+    }
+}
