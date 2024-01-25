2462bb987cdd4a2bba8d46ab60ba54d85ed09e0a
Paulo Victor <148218637+PauloVictorRodrigues@users.noreply.github.com>
2024-01-25 08:46:33 -0300

    Criação do PlanoAssinatura na Models

---
+++ Academia/Models/PlanoAssinatura.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class PlanoAssinatura
+    {
+        public int Id { get; set; }
+        public string TipoPlano { get; set; }
+        public double ValorPlano { get; set; }
+        public double Desconto { get; set; }
+    }
+}
