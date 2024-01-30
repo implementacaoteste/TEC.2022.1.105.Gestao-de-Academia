29d383e7cd664bec36b901f3c2047bb3f75e6592
Robson Souza <148218643+RobsonDSouza@users.noreply.github.com>
2024-01-30 10:59:07 -0300

    Itens recuperados da lixeira

--- Academia/UIGestaoAcademia/FormPrincipal.Designer.cs
+++ Academia/UIGestaoAcademia/FormPrincipal.Designer.cs
@@ -51,6 +51,11 @@
             // 
             menuStrip1.ImageScalingSize = new Size(20, 20);
             menuStrip1.Items.AddRange(new ToolStripItem[] { cadastroToolStripMenuItem, planoAssinaturaToolStripMenuItem, vENDASToolStripMenuItem, comprasToolStripMenuItem, exercicioToolStripMenuItem });
+
+            menuStrip1.Items.AddRange(new ToolStripItem[] { cadastroToolStripMenuItem, vENDASToolStripMenuItem, comprasToolStripMenuItem, fornecedorToolStripMenuItem1, exercicioToolStripMenuItem, planoAssinaturaToolStripMenuItem });
+
+            menuStrip1.Items.AddRange(new ToolStripItem[] { cadastroToolStripMenuItem, vENDASToolStripMenuItem, comprasToolStripMenuItem, fornecedorToolStripMenuItem1, exercicioToolStripMenuItem });
+
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
             menuStrip1.Size = new Size(856, 28);
@@ -217,4 +222,3 @@
         private ToolStripMenuItem planoAssinaturaToolStripMenuItem;
     }
 }
-
