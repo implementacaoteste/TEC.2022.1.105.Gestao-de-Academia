952b0408f4ea6ac8a2fc46c0c474a99e742a41c4
Guilherme De Jesus Rosa <148218502+Guilhermejr02@users.noreply.github.com>
2024-02-09 10:57:54 -0300

    Inicio do cálculo de juros

--- Academia/Models/ControleDebito.cs
+++ Academia/Models/ControleDebito.cs
@@ -2,6 +2,7 @@
 {
     public class ControleDebito
     {
+        private double juros;
         public int Id { get; set; }
         public int ClienteId { get; set; }
         public string NomeCliente
@@ -37,9 +38,34 @@
         public DateTime DataLancamento { get; set; }
         public DateTime DataVencimento { get; set; }
         public DateTime DataPagamento { get; set; }
-        public double Juros { get; set; }
+        public double Juros
+        {
+            get
+            {
+                double meses = DataVencimento.CompareTo(DateTime.Today);
+                if (meses > 0 && meses < 1)
+                    meses = 1;
+                else
+                    meses = Math.Truncate(((DateTime.Today - DataVencimento).TotalDays) / 30);
+
+                if (DataVencimento < DateTime.Today)
+                    return Math.Pow(1 + juros / 100, meses) * (ValorDebito + Acrescimo - Desconto);
+                return 0;
+            }
+            set
+            {
+                juros = value;
+            }
+        }
         public double Desconto { get; set; }
         public double Acrescimo { get; set; }
+        public double ValorAPagar
+        {
+            get
+            {
+                return ValorDebito + Acrescimo - Desconto + Juros;
+            }
+        }
         public string Descricao { get; set; }
         public int QuantidadeParcelas { get; set; }
         public FormaPagamento FormaPagamento { get; set; }
